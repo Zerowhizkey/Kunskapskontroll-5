f7b2ce813d541caac6289a6989f80e9b77ed63a3
marcus piri <marcus.piri@cmeducations.com>
2022-01-25 09:21:46 +0100

    cleaned up all the code, fixed CSS also maped JS and CSS

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,9 +1,9 @@
 // snowpack.config.mjs
 // Example: Using Snowpack's built-in bundling support
 export default {
-    optimize: {
-      bundle: true,
-      minify: true,
-      target: 'es2018',
-    },
-  };
+  optimize: {
+    bundle: true,
+    minify: true,
+    target: 'es2018',
+  },
+};
